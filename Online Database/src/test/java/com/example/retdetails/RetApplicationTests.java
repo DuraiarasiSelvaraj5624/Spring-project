4f453c0602647733ab90b1eb7627c0bcd0afc4c4
DuraiarasiSelvaraj5624 <115608776+DuraiarasiSelvaraj5624@users.noreply.github.com>
2023-04-26 10:34:27 +0530

    Online Database

---
+++ Online Database/src/test/java/com/example/retdetails/RetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
